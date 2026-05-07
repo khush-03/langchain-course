d1f05a07323c58bb13a4e7a041da6269bf9873a9
AS-MAC-0722 <as-mac-0722@AS-MAC-0722s-MacBook-Air.local>
2026-05-07 16:06:57 +0530

    langchain to summarise text

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from dotenv import load_dotenv
-
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 import os
 
 load_dotenv()
 def main():
     print("Hello from langchain-course!")
 
+    information="""
+   hi i am elon baba
+    """
+    summary_template="""
+    given the information {information} about a person i want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
 
 if __name__ == "__main__":
     main()
